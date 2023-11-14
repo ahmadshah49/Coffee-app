377529a983aa79c86cbcc85595e9f205a792a4c6
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2023-11-14 17:44:04 +0500

    coffee-shop-app

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,11 @@ import About from './about/page'
 import Menu from './menu/page'
 import Team from './team/page'
 import Play from './autoplay/page'
+import Projecs from './projecs/page'
+import Testimonial from './testimonial/page'
+import Contact from './contact/page'
+import Btn2 from './components/btn'
+import Footer from './footer/page'
 export default function Home() {
   return (
  <main id='home'>
@@ -29,6 +34,18 @@ export default function Home() {
  <Team title="marcelluss h. " chef="Senior Chef" img="https://images.unsplash.com/photo-1592498546551-222538011a27?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
  </div>
  <Play/>
+ <Projecs/>
+ <Testimonial/>
+ <Contact/>
+ <div className= ' bg-[#101010] py-10 flex flex-col md:flex-row items-center justify-center gap-14 px-4'>
+  <h1 className='text-4xl text-white font-bold' >Looking For Best Resturent ?</h1>
+  <div className='flex pt-6'> 
+    <input type="email"  className='p-1 outline-none' placeholder='Enter Your Email Address' />
+    <Btn2 btn="Send Message"/>
+  </div>
+  <hr />
+ </div>
+<Footer/>
  </main>
   )
 }
